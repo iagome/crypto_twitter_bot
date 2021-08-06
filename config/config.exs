33c7ec145d8d0d639b70580513d2fa970900441c
iago <iagoo_o@hotmail.com>
2021-08-06 00:20:20 -0300

    add dependencies and config

---
+++ config/config.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :extwitter, :oauth, [
+  consumer_key: System.get_env("CONSUMER_KEY"),
+  consumer_secret: System.get_env("CONSUMER_SECRET"),
+  access_token: System.get_env("ACCESS_TOKEN"),
+  access_token_secret: System.get_env("ACCESS_SECRET")
+]
