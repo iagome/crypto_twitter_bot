947a92f16372590c77e70f27ee2e64ab2641a522
iago <iagoo_o@hotmail.com>
2021-08-06 00:53:38 -0300

    build main modules

--- lib/crypto_twitter_bot.ex
+++ lib/crypto_twitter_bot.ex
@@ -3,16 +3,21 @@ defmodule CryptoTwitterBot do
   Documentation for `CryptoTwitterBot`.
   """
 
+  alias CryptoTwitterBot.CryptoFetch
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CryptoTwitterBot.hello()
-      :world
-
+  TODO
   """
-  def hello do
-    :world
+  def start do
+    # roda a cada X minutos (preciso pesquisar como fazer isso)
+    with {:ok, slp} <- CryptoFetch.slp_info,
+         {:ok, axs} <- CryptoFetch.axs_info,
+         {:ok, eth} <- CryptoFetch.eth_info do
+      IO.puts("-----> Caught all prices, building tweet!")
+    else
+      _ -> {:error, "Can't fetch prices"}
+    end
+
+
   end
 end
---
+++ lib/crypto_twitter_bot/crypto_fetch.ex
@@ -0,0 +1,17 @@
+defmodule CryptoTwitterBot.CryptoFetch do
+  @moduledoc """
+  Public documentation for `CryptoFetch`.
+  """
+
+  def slp_info do
+
+  end
+
+  def axs_info do
+
+  end
+
+  def eth_info do
+
+  end
+end
